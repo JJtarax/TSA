3bf81700bf414a972120cf3b1c2f1da1269b542c
Jas Jtarax <raymajtarax@gmail.com>
2024-01-07 23:10:37 -0500

    wasd

---
+++ exPlayer.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export var movement_data : PlayerMovementData
+
+var gravity = ProjectSettings.get_settings("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left","ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	update_animation(input_axis)
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= -
+	if just_left_ledge:
+		coyote_jump_timer.start()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_jump():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = movement_data.jumps_velocity
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jumps_velocity /2:
+			velocity.y =  movement_data.jumps_velocity / 2
+
+func handle_acceleration(input_axis, delta)
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func apply_friction(input_axis, delta):
+	if input_axis ==0:
+		velocity.x = move_toward(velocity.x,0, movement_data.friction * delta)
+
+func update_animation(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
